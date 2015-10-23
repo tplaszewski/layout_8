1d37f8005b3b5dcdad746205240f5c4afffa81b1
tplaszewski <t.plaszewski@gmail.com>
2015-10-23 14:20:23 +0200

    finish

--- src/js/main.js
+++ src/js/main.js
@@ -52,23 +52,146 @@ jQuery(document).ready(function( $ ) {
 window.onload = function () {
             var styles = [
     {
-        "featureType": "all",
+        "featureType": "water",
         "elementType": "all",
         "stylers": [
             {
-                "invert_lightness": true
+                "hue": "#71d6ff"
             },
             {
-                "saturation": 10
+                "saturation": 100
             },
             {
-                "lightness": 30
+                "lightness": -5
             },
             {
-                "gamma": 0.5
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#ffffff"
             },
             {
-                "hue": "#435158"
+                "saturation": -100
+            },
+            {
+                "lightness": 100
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "hue": "#ffffff"
+            },
+            {
+                "saturation": 0
+            },
+            {
+                "lightness": 100
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "hue": "#deecec"
+            },
+            {
+                "saturation": -73
+            },
+            {
+                "lightness": 72
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "hue": "#bababa"
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 25
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "hue": "#e3e3e3"
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 0
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "hue": "#ffffff"
+            },
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 100
+            },
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "hue": "#59cfff"
+            },
+            {
+                "saturation": 100
+            },
+            {
+                "lightness": 34
+            },
+            {
+                "visibility": "on"
             }
         ]
     }
